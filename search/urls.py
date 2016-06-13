9782dbec179a895fe5520bf85367d60d3fa9d0a1
Moisés <moises.lodeiro@gmail.com>
2016-06-13 12:35:29 +0100

    Changes in responsive dessign and settings.py

--- search/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from .views import searchMap, createRoute, getTracking, setTracking
-
-urlpatterns = [
-    url(r'^map/', searchMap),
-    url(r'^createRoute/', createRoute, name='createRoute'),
-    url(r'^getTracking/', getTracking, name='getTracking'),
-    url(r'^setTracking/', setTracking, name='setTracking'),
-]
