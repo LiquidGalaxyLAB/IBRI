3def2aa025d916fae6a011ce3c0bce9a855de39e
Moisés <moises.lodeiro@gmail.com>
2016-06-22 09:46:34 +0100

    Changed the way to represent the KML. Now is a polyline

--- search/views.py
+++ search/views.py
@@ -2,6 +2,7 @@
 import json
 from math import sqrt
 
+import simplekml
 from config.models import Config
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
@@ -76,8 +77,6 @@ def setTracking(request):
         if d['scan'] != None:
             wp.signalFound = int(d['scan'])
             print("[ Beacon Found ] - Checking #" + str(d['scan']))
-        #else:
-        #    print "----" + str(d['scan'])
 
         wp.visited = True
         wp.photo = d['photo']
@@ -89,7 +88,160 @@ def setTracking(request):
 
 
 
+
+
 def createRoute(request):
+
+    if(request.method == 'POST'):
+
+        dec = json.loads(AESCipher(b'preshared_key012').decrypt(request.POST['msg']))
+
+        base = json.loads(dec['base'])
+        coords = json.loads(dec['wayPoints'])
+        nearPoint = json.loads(dec['nearPoint'])
+        userPK = json.loads(dec['insearch'])
+
+        nAreas = len(coords)
+        totalElements = 0
+        for i in coords:
+            totalElements += len(i)
+
+        size = int(sqrt(totalElements))
+        nElements = int(math.floor(math.sqrt(len(coords[0]))))
+
+        searchArea = [[[] for _ in range(nElements)] for _ in range(nAreas)]
+
+        ov1 = 0
+        ov2 = 0
+
+        for i in range(nAreas):
+            for j in range(nElements):
+
+                col = j * size
+
+                if(ov1 == 0):
+                    searchArea[i][j] = coords[i][(col-ov2):(col-ov2)+size]
+                else:
+                    searchArea[i][j] = coords[i][0:ov1]
+                    ov2 = size - ov1
+                    ov1 = 0
+
+            if(len(searchArea[i][j]) == 0):
+                searchArea[i].pop()
+                j -= 1
+
+            if(size - len(searchArea[i][j]) > 0):
+                ov1 = size - len(searchArea[i][j])
+                ov2 = 0
+            else:
+                ov1 = 0
+                ov2 = 0
+
+        coord = []
+
+        x = 0
+
+        for i in range(nAreas):
+            coord.append([])
+            for j in searchArea[i]:
+                for k in j:
+                    coord[i].append([(x / size), (x % size)])
+                    x += 1
+
+        ac = 0
+        route = []
+
+        for i in range(nAreas):
+
+            n, D = mk_matrix(coord[i], distL2)
+            tour = nearest_neighbor(n, (((nearPoint[i][0] * size)-ac) + nearPoint[i][1]), D)
+
+            for j in range(len(tour)):
+
+                tmpValue = tour[j] + (size - len(searchArea[i][0]))
+                col = tmpValue / size
+
+                if col == 0 and i > 0:
+                    row = (tmpValue % size) - (size - len(searchArea[i][0]))
+                    print(str(tour[j]) + " = " + str(col) + "," + str((tmpValue % size) - (size - len(searchArea[i][0]))))
+                else:
+                    row = tmpValue % size
+                    print(str(tour[j])+" = "+str(col)+","+str(tmpValue % size))
+
+                tour[j] = searchArea[i][col][row]
+
+            ac += len(coord[i])
+            route.append(tour)
+
+
+        m = Mission()
+        m.save()
+
+        for userId in range(0, len(userPK)):
+            m.inSearch.add(Clients.objects.get(pk=userPK[userId]))
+
+        try:
+            drones = Drone.objects.all()[0]
+        except:
+            print("\033[91m Oops!  No drones found un database")
+            return HttpResponse('configerror')
+
+        # TODO quitar la variable drone_secuencial
+        drone_secuencial = 0
+        from os import mkdir
+        for r in route:
+
+            # TODO cambiar para que la elección del dron la haga el usuario y no sea secuencial
+            rm = Route(mission=m, drone=Drone.objects.all()[drone_secuencial], baseLat=base[0], baseLng=base[1])
+            rm.save()
+            tmpRoute = []
+            tmpCounter = 0
+
+            kmlName = 'IBRI'+str(m.id)+'R'+str(rm.id)
+            kml = Kml(name=kmlName)
+
+            kml.newpoint(name="Base", coords=[(base[1], base[0])])
+
+            pnt = kml.newlinestring(name='Route {}'.format(tmpCounter))
+            coords = []
+
+            for wp in r:
+
+                coords.append((wp[1], wp[0]))
+
+                #pnt = kml.newpoint(name='Point {}'.format(tmpCounter))
+                #pnt.coords = [(wp[1], wp[0])]
+
+                #if settings.KML_ICON != '':
+                #    pnt.style.iconstyle.icon.href = settings.KML_ICON
+
+                tmpRoute.append(WayPoint(route=rm, lat=wp[0], lng=wp[1], ref=tmpCounter))
+                tmpCounter += 1
+
+            pnt.coords = coords
+
+            tmpRoute.append(WayPoint(route=rm, lat=base[0], lng=base[1], ref=tmpCounter))
+            kml.newpoint(name="Back to base", coords=[(base[1], base[0])])
+
+            WayPoint.objects.bulk_create(tmpRoute)
+
+            place = settings.KML_DIR + '/IBRI' + str(m.id)
+
+            try:
+                os.mkdir(place)
+            except:
+                pass
+
+            kml.save(place + '/' + kmlName + '.kml')  # Saving
+
+            drone_secuencial += 1
+
+        preshared = b'preshared_key012'
+        return HttpResponse(AESCipher(preshared).encrypt(json.dumps([m.pk, route])))
+
+
+
+def createRoute_deprecated(request):
 
     if(request.method == 'POST'):
 
