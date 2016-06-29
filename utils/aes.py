d80aa9c26080c5ec705313880b6343aa62b82b87
Moisés <moises.lodeiro@gmail.com>
2016-06-29 14:25:59 +0100

    Added another AES class to interact with android aes

--- utils/aes.py
+++ utils/aes.py
@@ -28,6 +28,33 @@ class AESCipher(object):
         return self._unpad(aes.decrypt(encrypted[self.BLOCK_SIZE:]))
 
 
+class JavaAESCipher:
+
+    def __init__(self, key):
+        self.bs = 16
+        self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self, message):
+        message = self._pad(message)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(message)).decode('utf-8')
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
+
+
+
 if __name__ == '__main__':
 
     BLOCK_SIZE = 16
