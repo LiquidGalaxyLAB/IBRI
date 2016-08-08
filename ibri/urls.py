ef5f7b11d8b296490250cbac85a0a2abbc4621ca
Moisés Lodeiro <moises.lodeiro@gmail.com>
2016-08-08 14:02:42 +0100

    Added jquery to local files

--- ibri/urls.py
+++ ibri/urls.py
@@ -8,15 +8,13 @@ import config.urls
 from search.views import searchMap, createRoute, getTracking, setTracking, setDroneTracking, getDroneMissionData
 import clients.urls
 from clients.views import ulogin
-from django.contrib.auth.views import logout
 
 from utils.weatherApi import getWeather
 
 urlpatterns = [
 	url(r'login/', ulogin, name="login"),
-    url(r'^ulogout/', logout, {'next_page': '/'}, name='ulogout'),
     url(r'^u/', include(admin.site.urls)),
-    url(r'^admin/', include(config.urls), name='adminurl'),
+    url(r'^admin/', include(config.urls)),
     url(r'^p/', include(clients.urls)),
     url(r'^createRoute/$', createRoute, name='createRoute'),
     url(r'^getTracking/$', getTracking, name='getTracking'),
